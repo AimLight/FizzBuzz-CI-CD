659209315a5720c1295d03bf996cb11f18282eb0
iorana <olegpolivin@gmail.com>
2020-12-03 10:39:33 +0100

    My great fizzbuzz solution

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,13 @@
-# Here I start the solution to the fizz buzz problem #######
+import scipy
+import pandas
+import numpy as np 
+
+import matplotlib
+from matplotlib import pyplot as plt
+
+####### Here I start the solution to the fizz buzz problem #######
 def fizz_buzz(n: int) -> str:
-    if n % 15 == 0:
-        return "fizzbuzz"
-    elif n % 5 == 0:
-        return "buzz"
-    elif n % 3 == 0:
-        return "fizz"
-    else:
-        return str(n)
-
-
-print(fizz_buzz(10))
+    if n % 15 == 0: return 'fizzbuzz'
+    elif n % 5 == 0: return 'buzz'
+    elif n % 3 == 0: return 'fizz'
+    else: return str(n)
